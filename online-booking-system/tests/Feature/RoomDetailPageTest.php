dc2d916886c7b55784cc2a7715c6555c0b13c417
ruwen <ruwenaraojo@gmail.com>
2026-09-23 14:44:07 +0800

    improved design

--- online-booking-system/tests/Feature/RoomDetailPageTest.php
+++ online-booking-system/tests/Feature/RoomDetailPageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Room;
 use Tests\TestCase;
 
 class RoomDetailPageTest extends TestCase
@@ -15,7 +16,31 @@ class RoomDetailPageTest extends TestCase
         $response->assertSee('Book This Room');
     }
 
-    public function test_homepage_has_four_clickable_room_links(): void
+    public function test_homepage_uses_real_room_data_for_featured_rooms(): void
+    {
+        Room::query()->delete();
+
+        Room::query()->create([
+            'room_number' => '999',
+            'room_type' => 'Ocean View Suite',
+            'price' => 4200.00,
+            'floor' => '9',
+            'status' => 'available',
+            'cleaning_status' => 'clean',
+            'description' => 'A scenic room with a bright, calming interior.',
+            'image' => 'ocean-view-suite.jpg',
+            'capacity' => 2,
+        ]);
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertSee('Ocean View Suite');
+        $response->assertSee('images/ocean-view-suite.jpg');
+        $response->assertSee(route('accommodation.room', ['slug' => 'ocean-view-suite']));
+    }
+
+    public function test_homepage_has_five_clickable_room_links(): void
     {
         $response = $this->get('/');
 
@@ -24,6 +49,7 @@ class RoomDetailPageTest extends TestCase
         $response->assertSee('Executive Room');
         $response->assertSee('Presidential Room');
         $response->assertSee('Standard Room');
+        $response->assertSee('Garden Suite');
         $response->assertSee(route('accommodation.room', ['slug' => 'deluxe-room']));
     }
 }
